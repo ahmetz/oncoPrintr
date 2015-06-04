cb7854b269dbc3b65276ef975cfc4f8e2b1cf0c9
Zehir <zehira@lpth0112.mskcc.org>
2015-06-04 18:05:22 -0400

    removed paste.matrix as a seperate function

--- R/oncoPrint.R
+++ R/oncoPrint.R
@@ -20,50 +20,6 @@ oncoPrint <- function(df, sort=TRUE, convert = TRUE, total_samples = NA, geneNam
   # This is the plotting function
   library(reshape2)
   library(dplyr)
-
-
-
-  paste.matrix <- function(M1, M2){
-    rownames1 <- row.names(M1)
-    rownames2 <- row.names(M2)
-    colnames1 <- colnames(M1)
-    colnames2 <- colnames(M2)
-    row_names_all <- unique(c(rownames1, rownames2))
-    col_names_all <- unique(c(colnames1, colnames2))
-    
-    n = length(row_names_all)
-    m = length(col_names_all)
-    ret <- matrix(rep(NA, n *m), ncol = m)
-    colnames(ret) <- col_names_all
-    row.names(ret) <- row_names_all
-    
-    for(row in row_names_all){
-      for(col in col_names_all){
-        x = NA
-        y = NA
-        if(row %in% rownames1 && col %in% colnames1){
-          x = M1[row, col]
-        }
-        if(row %in% rownames2 && col %in% colnames2){
-          y = M2[row, col]
-        }
-        
-        if(is.na(x) && is.na(y)){
-          ret[row, col] <- NA
-        }else if(is.na(x) && !is.na(y)){
-          ret[row, col] <- y
-        }else if(!is.na(x) && is.na(y)){
-          ret[row, col]<- x
-        }else if(!is.na(x) && !is.na(y)){
-          ret[row, col] <- paste(x, y, sep=",")
-        }
-      }
-    }
-    ret <- as.matrix(ret)
-    colnames(ret) <- col_names_all
-    row.names(ret) <- row_names_all
-    return(ret)
-  }
   
   remove_duplicates <- function(df){
     choose_alteration_type <- function(x){
