f988c8ddf1b82423d4779cccb313426028f1f324
Ahmet Zehir <ahmetzehir@gmail.com>
2016-01-06 21:28:25 -0500

    Update memoSort.R

--- R/memoSort.R
+++ R/memoSort.R
@@ -11,7 +11,7 @@
 #' @examples TODO2
 memoSort <- function(M, geneOrder = geneOrder, annotations = NA, annotation_order = NA) {
   print(geneOrder)
-  cat (M)
+  cat (row.names(M), "\n")
   M <- M[geneOrder, ]
   if(!is.na(annotations)){
     colnames(annotations) <- c("sample", "class")
